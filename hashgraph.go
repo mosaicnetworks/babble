e8fe5bb0f76ef6bf100810e1e415b33100f8a83c
arrivets <arrivets@gmail.com>
2017-01-19 08:02:00 +0000

    see and fork test

--- hashgraph.go
+++ hashgraph.go
@@ -15,6 +15,10 @@ limitations under the License.
 */
 package hashgraph
 
+import (
+	"reflect"
+)
+
 type Hashgraph struct {
 	Events map[string]Event //hash => event
 }
@@ -25,7 +29,7 @@ func NewHashgraph() Hashgraph {
 	}
 }
 
-//returns true if y is an ancestor of x
+//true if y is an ancestor of x
 func (h *Hashgraph) Ancestor(x, y string) bool {
 	if x == "" {
 		return false
@@ -42,4 +46,68 @@ func (h *Hashgraph) Ancestor(x, y string) bool {
 		return false
 	}
 	return h.Ancestor(ex.Body.Parents[0], y) || h.Ancestor(ex.Body.Parents[1], y)
+}
+
+//true if y is a self-ancestor of x
+func (h *Hashgraph) SelfAncestor(x, y string) bool {
+	if x == "" {
+		return false
+	}
+	ex, ok := h.Events[x]
+	if !ok {
+		return false
+	}
+	if x == y {
+		return true
+	}
+	_, ok = h.Events[y]
+	if !ok {
+		return false
+	}
+	return h.SelfAncestor(ex.Body.Parents[0], y)
+}
+
+//true if x detects a fork under y. also returns the hash of the event which
+//is not a self-ancestor of y and caused the fork
+func (h *Hashgraph) DetectFork(x, y string) bool {
+	if x == "" || y == "" {
+		return false
+	}
+	_, ok := h.Events[x]
+	if !ok {
+		return false
+	}
+	ey, ok := h.Events[y]
+	if !ok {
+		return false
+	}
+
+	//filter events satisfying the following criteria:
+	//- same creator as y
+	//- ancestors of x
+	filteredEvents := []Event{}
+	for hash, event := range h.Events {
+		if reflect.DeepEqual(event.Body.Creator, ey.Body.Creator) &&
+			h.Ancestor(x, hash) {
+			filteredEvents = append(filteredEvents, event)
+		}
+	}
+	for i := 0; i < len(filteredEvents)-1; i++ {
+		a := filteredEvents[i].Hex()
+		for j := i + 1; j < len(filteredEvents); j++ {
+			b := filteredEvents[j].Hex()
+			if !((h.SelfAncestor(a, b)) || h.SelfAncestor(b, a)) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+//true if x sees y
+func (h *Hashgraph) See(x, y string) bool {
+	if !h.Ancestor(x, y) {
+		return false
+	}
+	return !h.DetectFork(x, y)
 }
--- hashgraph_test.go
+++ hashgraph_test.go
@@ -65,21 +65,21 @@ func initHashgraph() (Hashgraph, map[string]string) {
 	}
 
 	event01 := NewEvent([][]byte{},
-		[]string{nodes[0].Events[0].Hex(), nodes[1].Events[0].Hex()},
+		[]string{nodes[0].Events[0].Hex(), nodes[1].Events[0].Hex()}, //e0 and e1
 		nodes[0].Pub)
 	event01.Sign(nodes[0].Key)
 	nodes[0].Events = append(nodes[0].Events, event01)
 	index["e01"] = event01.Hex()
 
 	event20 := NewEvent([][]byte{},
-		[]string{nodes[0].Events[1].Hex(), nodes[2].Events[0].Hex()},
+		[]string{nodes[2].Events[0].Hex(), nodes[0].Events[1].Hex()}, //e2 and e01
 		nodes[2].Pub)
 	event20.Sign(nodes[2].Key)
 	nodes[2].Events = append(nodes[2].Events, event20)
 	index["e20"] = event20.Hex()
 
 	event12 := NewEvent([][]byte{},
-		[]string{nodes[1].Events[0].Hex(), nodes[2].Events[1].Hex()},
+		[]string{nodes[1].Events[0].Hex(), nodes[2].Events[1].Hex()}, //e1 and e20
 		nodes[1].Pub)
 	event12.Sign(nodes[1].Key)
 	nodes[1].Events = append(nodes[1].Events, event12)
@@ -142,6 +142,220 @@ func TestAncestor(t *testing.T) {
 	//false positive
 	if h.Ancestor(index["e01"], index["e2"]) {
 		t.Fatal("e2 should not be ancestor of e01")
+	}
+
+}
+
+func TestSelfAncestor(t *testing.T) {
+	h, index := initHashgraph()
+
+	//1 generation
+	if !h.SelfAncestor(index["e01"], index["e0"]) {
+		t.Fatal("e0 should be self ancestor of e01")
+	}
+	if !h.SelfAncestor(index["e20"], index["e2"]) {
+		t.Fatal("e2 should be self ancestor of e20")
+	}
+	if !h.SelfAncestor(index["e12"], index["e1"]) {
+		t.Fatal("e1 should be self ancestor of e12")
+	}
+
+	//1 generation false negatives
+	if h.SelfAncestor(index["e01"], index["e1"]) {
+		t.Fatal("e1 should not be self ancestor of e01")
+	}
+	if h.SelfAncestor(index["e20"], index["e01"]) {
+		t.Fatal("e01 should not be self ancestor of e20")
+	}
+	if h.SelfAncestor(index["e12"], index["e20"]) {
+		t.Fatal("e20 should not be self ancestor of e12")
+	}
+
+	//2 generation false negative
+	if h.SelfAncestor(index["e20"], index["e0"]) {
+		t.Fatal("e0 should not be self ancestor of e20")
+	}
+	if h.SelfAncestor(index["e12"], index["e2"]) {
+		t.Fatal("e2 should not be self ancestor of e12")
+	}
+
+}
+
+/*
+|   e12    |
+|    | \   |
+|    |   \ |
+|    |    e20
+|    |   / |
+|    | /   |
+|    /     |
+|  / |     |
+e01  |     |
+| \  |     |
+|   \|     |
+|    |\    |
+|    |  \  |
+e0   e1 (a)e2
+0    1     2
+
+Node 2 Forks; events a and e2 are both created by node2, they are not self-parents
+and yet they are both ancestors of event e20
+*/
+func initForkHashgraph() (Hashgraph, map[string]string) {
+	n := 3
+	index := make(map[string]string)
+
+	nodes := []struct {
+		Pub    []byte
+		PubHex string
+		Key    *ecdsa.PrivateKey
+		Events []Event
+	}{}
+
+	for i := 0; i < n; i++ {
+		key, _ := crypto.GenerateECDSAKey()
+		pub := crypto.FromECDSAPub(&key.PublicKey)
+		pubHex := fmt.Sprintf("0x%X", pub)
+		event := NewEvent([][]byte{}, []string{"", ""}, pub)
+		event.Sign(key)
+		name := fmt.Sprintf("e%d", i)
+		index[name] = event.Hex()
+		events := []Event{event}
+		node := struct {
+			Pub    []byte
+			PubHex string
+			Key    *ecdsa.PrivateKey
+			Events []Event
+		}{Pub: pub, PubHex: pubHex, Key: key, Events: events}
+		nodes = append(nodes, node)
+	}
+
+	//a and e2 need to have different hashes
+	eventA := NewEvent([][]byte{[]byte("yo")}, []string{"", ""}, nodes[2].Pub)
+	eventA.Sign(nodes[2].Key)
+	nodes[2].Events = append(nodes[2].Events, eventA)
+	index["a"] = eventA.Hex()
+
+	event01 := NewEvent([][]byte{},
+		[]string{nodes[0].Events[0].Hex(), nodes[2].Events[1].Hex()}, //e0 and A
+		nodes[0].Pub)
+	event01.Sign(nodes[0].Key)
+	nodes[0].Events = append(nodes[0].Events, event01)
+	index["e01"] = event01.Hex()
+
+	event20 := NewEvent([][]byte{},
+		[]string{nodes[2].Events[0].Hex(), nodes[0].Events[1].Hex()}, //e2 and e01
+		nodes[2].Pub)
+	event20.Sign(nodes[2].Key)
+	nodes[2].Events = append(nodes[2].Events, event20)
+	index["e20"] = event20.Hex()
+
+	event12 := NewEvent([][]byte{},
+		[]string{nodes[1].Events[0].Hex(), nodes[2].Events[2].Hex()}, //e1 and e20
+		nodes[1].Pub)
+	event12.Sign(nodes[1].Key)
+	nodes[1].Events = append(nodes[1].Events, event12)
+	index["e12"] = event12.Hex()
+
+	hashgraph := NewHashgraph()
+	for _, node := range nodes {
+		for _, ev := range node.Events {
+			hashgraph.Events[ev.Hex()] = ev
+		}
+	}
+	return hashgraph, index
+}
+
+func TestDetectFork(t *testing.T) {
+	h, index := initForkHashgraph()
+
+	//1 generation
+	fork := h.DetectFork(index["e20"], index["a"])
+	if !fork {
+		t.Fatal("e20 should detect a fork under a")
+	}
+	fork = h.DetectFork(index["e20"], index["e2"])
+	if !fork {
+		t.Fatal("e20 should detect a fork under e2")
+	}
+	fork = h.DetectFork(index["e12"], index["e20"])
+	if !fork {
+		t.Fatal("e12 should detect a fork under e20")
+	}
+
+	//2 generations
+	fork = h.DetectFork(index["e12"], index["a"])
+	if !fork {
+		t.Fatal("e12 should detect a fork under a")
+	}
+	fork = h.DetectFork(index["e12"], index["e2"])
+	if !fork {
+		t.Fatal("e12 should detect a fork under e2")
+	}
+
+	//false negatives
+	fork = h.DetectFork(index["e01"], index["e0"])
+	if fork {
+		t.Fatal("e01 should not detect a fork under e0")
+	}
+	fork = h.DetectFork(index["e01"], index["a"])
+	if fork {
+		t.Fatal("e01 should not detect a fork under 'a'")
+	}
+	fork = h.DetectFork(index["e01"], index["e2"])
+	if fork {
+		t.Fatal("e01 should not detect a fork under e2")
+	}
+	fork = h.DetectFork(index["e20"], index["e01"])
+	if fork {
+		t.Fatal("e20 should not detect a fork under e01")
+	}
+	fork = h.DetectFork(index["e12"], index["e01"])
+	if fork {
+		t.Fatal("e12 should not detect a fork under e01")
+	}
+}
+
+func TestSee(t *testing.T) {
+	h, index := initForkHashgraph()
+
+	if !h.See(index["e01"], index["e0"]) {
+		t.Fatal("e01 should see e0")
+	}
+	if !h.See(index["e01"], index["a"]) {
+		t.Fatal("e01 should see 'a'")
+	}
+	if !h.See(index["e20"], index["e0"]) {
+		t.Fatal("e20 should see e0")
+	}
+	if !h.See(index["e20"], index["e01"]) {
+		t.Fatal("e20 should see e01")
+	}
+	if !h.See(index["e12"], index["e01"]) {
+		t.Fatal("e12 should see e01")
+	}
+	if !h.See(index["e12"], index["e0"]) {
+		t.Fatal("e12 should see e0")
+	}
+	if !h.See(index["e12"], index["e1"]) {
+		t.Fatal("e12 should see e1")
+	}
+
+	//fork
+	if h.See(index["e20"], index["a"]) {
+		t.Fatal("e20 should not see 'a' because of fork")
+	}
+	if h.See(index["e20"], index["e2"]) {
+		t.Fatal("e20 should not see e2 because of fork")
+	}
+	if h.See(index["e12"], index["a"]) {
+		t.Fatal("e12 should not see 'a' because of fork")
+	}
+	if h.See(index["e12"], index["e2"]) {
+		t.Fatal("e12 should not see e2 because of fork")
+	}
+	if h.See(index["e12"], index["e20"]) {
+		t.Fatal("e12 should not see e20 because of fork")
 	}
 
 }
