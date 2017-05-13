c218ab6d92980429c07ed3664b7a08d08205737a
Martin Arrivets <martin@babble.io>
2017-05-13 15:29:04 +0100

    sec terraform

--- terraform/example.tf
+++ terraform/example.tf
@@ -1,10 +1,14 @@
 provider "aws" {
-  access_key = "AKIAIREUC5KEUFOVU62Q"
-  secret_key = "kaWJBy6LZBOS39NQX6/Cuk03Q5fM4+WkyKSCKJ9k"
-  region     = "us-east-1"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-656be372"
-  instance_type = "t1.micro"
+  ami           = "ami-a8d2d7ce"
+  instance_type = "t2.micro"
+}
+
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.example.id}"
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {
+  default = "eu-west-1"
+}
