9c3ce90a4fd18285d375952075dd6503e4498060
Martin Arrivets <martin@babble.io>
2017-05-17 15:59:23 +0200

    Terraform AWS deployment

--- terraform/example.tf
+++ terraform/example.tf
@@ -1,14 +1,101 @@
 provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
-  region     = "${var.region}"
+  region     = "eu-west-2"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-a8d2d7ce"
+resource "aws_subnet" "babblenet" {
+  vpc_id     = "${var.vpc}"
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch="true"
+
+  tags {
+    Name = "Testnet"
+  }
+}
+
+resource "aws_security_group" "babblesec" {
+    name = "babblesec"
+    description = "Babble internal traffic + maintenance."
+
+    vpc_id     = "${var.vpc}"
+
+    // These are for internal traffic
+    ingress {
+        from_port = 0
+        to_port = 65535
+        protocol = "tcp"
+        self = true
+    }
+
+    ingress {
+        from_port = 0
+        to_port = 65535
+        protocol = "udp"
+        self = true
+    }
+
+    // These are for maintenance
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    
+    ingress {
+        from_port = -1
+        to_port = -1
+        protocol = "icmp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    // This is for outbound internet access
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_instance" "server" {
+  count = "${var.servers}"
+  
+  ami = "ami-6d7d6a09" //custom ami with ubuntu + babble + dummy
   instance_type = "t2.micro"
+
+  subnet_id = "${aws_subnet.babblenet.id}"
+  vpc_security_group_ids  = ["${aws_security_group.babblesec.id}"]
+  private_ip = "10.0.1.${10+count.index}"
+
+  key_name = "${var.key_name}"
+  connection {
+    user = "ubuntu"
+    private_key = "${file("${var.key_path}")}"
+  }
+
+  provisioner "file" {
+    source      = "conf/node${count.index +1}"
+    destination = "babble_conf" 
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "nohup /home/ubuntu/bin/babble run --datadir=/home/ubuntu/babble_conf --cache_size=50000 --tcp_timeout=1000 --heartbeat=50 --node_addr=10.0.1.${10+count.index}:1337 --no_client=true > logs 2>&1 &",
+      "sleep 1",
+      //"/home/ubuntu/bin/babble run --datadir=/home/ubuntu/babble_conf --cache_size=50000 --tcp_timeout=1000 --heartbeat=50 --node_addr=10.0.1.${10+count.index}:1337 --proxy_addr=127.0.0.1:1338 --client_addr=127.0.0.1:1339", 
+      //"/home/ubuntu/bin/dummy --name=client${count.index +1} --client_addr=127.0.0.1:1339 --proxy_addr=127.0.0.1:1338 --log_level=info",
+    ]
+  }
+
+  #Instance tags
+  tags {
+      Name = "node${count.index}"
+  }
+
 }
 
-resource "aws_eip" "ip" {
-  instance = "${aws_instance.example.id}"
+output "server_address" {
+    value = ["${aws_instance.server.*.public_ip}"]
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,22 @@
 variable "access_key" {}
 variable "secret_key" {}
-variable "region" {
-  default = "eu-west-1"
+
+variable "vpc" {
+  default = "vpc-7951fb10"
+}
+
+variable "ig" {
+  default = "igw-2d03de44"
+}
+
+variable "servers" {
+  default = 4
+}
+
+variable "key_name" {
+  description = "SSH key name in your AWS account for AWS instances."
+}
+
+variable "key_path" {
+  description = "Path to the private key specified by key_name."
 }
