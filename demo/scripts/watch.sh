91ce3d3089130e5dfdc91fa72e04c3188263ab30
arrivets <arrivets@gmail.com>
2018-01-06 16:18:36 +0000

    Version bump v0.1.4

--- demo/scripts/watch.sh
+++ demo/scripts/watch.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-
 N=${1:-4}
 
-watch -n 1 '
-for i in $(seq 1 '$N');
-do
-    curl -s -m 1 http://172.77.5.$i:80/Stats | \
-        tr -d "{}\"" | \
-        awk -F "," '"'"'{gsub (/[,]/," "); print;}'"'"'
-done;
-'
+docker run -it --rm --name=watcher --net=babblenet --ip=172.77.5.99 mosaicnetworks/watcher /watch.sh $N  
