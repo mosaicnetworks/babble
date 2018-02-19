0b1402bd30487bd4e703420480e137b739555a89
Stanislav Stoyanov <stanislav.stoyanov@orangedot.bg>
2018-02-19 23:43:38 +0200

    Initial android application

---
+++ demo/android/app/src/main/java/io/babble/mobile/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package io.babble.mobile.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    Node node;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
