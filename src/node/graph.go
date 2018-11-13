dc1575a6e7ee88c47523e26f18dd772493fbba50
champii <contact@champii>
2018-11-13 01:14:27 +0100

    Graph fixes for endless scroll

--- src/node/graph.go
+++ src/node/graph.go
@@ -19,6 +19,7 @@ func (g *Graph) GetParticipantEvents() map[string]map[string]*hg.Event {
 
 	store := g.Node.core.hg.Store
 	repertoire := g.Node.core.hg.Store.RepertoireByPubKey()
+	known := store.KnownEvents()
 
 	for _, p := range repertoire {
 		root, err := store.GetRoot(p.PubKeyHex)
@@ -27,7 +28,12 @@ func (g *Graph) GetParticipantEvents() map[string]map[string]*hg.Event {
 			panic(err)
 		}
 
-		evs, err := store.ParticipantEvents(p.PubKeyHex, root.SelfParent.Index)
+		skip := known[p.ID] - 30
+		if skip < 0 {
+			skip = -1
+		}
+
+		evs, err := store.ParticipantEvents(p.PubKeyHex, skip)
 
 		if err != nil {
 			panic(err)
@@ -44,8 +50,11 @@ func (g *Graph) GetParticipantEvents() map[string]map[string]*hg.Event {
 			-1,
 		)
 
+		isFirst := true
+
 		for _, e := range evs {
 			event, err := store.GetEvent(e)
+			ev := *event
 
 			if err != nil {
 				panic(err)
@@ -53,7 +62,12 @@ func (g *Graph) GetParticipantEvents() map[string]map[string]*hg.Event {
 
 			hash := event.Hex()
 
-			res[p.PubKeyHex][hash] = event
+			if isFirst {
+				isFirst = false
+				ev.Body.Parents = []string{}
+			}
+
+			res[p.PubKeyHex][hash] = &ev
 		}
 	}
 
@@ -63,14 +77,18 @@ func (g *Graph) GetParticipantEvents() map[string]map[string]*hg.Event {
 func (g *Graph) GetRounds() []*hg.RoundInfo {
 	res := []*hg.RoundInfo{}
 
-	round := 0
-
 	store := g.Node.core.hg.Store
+
+	round := store.LastRound() - 20
+
+	if round < 0 {
+		round = 0
+	}
 
 	for round <= store.LastRound() {
 		r, err := store.GetRound(round)
 
-		if err != nil || !r.IsQueued() {
+		if err != nil {
 			break
 		}
 
@@ -85,9 +103,13 @@ func (g *Graph) GetRounds() []*hg.RoundInfo {
 func (g *Graph) GetBlocks() []*hg.Block {
 	res := []*hg.Block{}
 
-	blockIdx := 0
-
 	store := g.Node.core.hg.Store
+
+	blockIdx := store.LastBlockIndex() - 10
+
+	if blockIdx < 0 {
+		blockIdx = 0
+	}
 
 	for blockIdx <= store.LastBlockIndex() {
 		r, err := store.GetBlock(blockIdx)
