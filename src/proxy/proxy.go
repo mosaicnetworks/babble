8debe20a0874efaae242e9ed8f71c1065da9c07d
arrivets <arrivets@gmail.com>
2019-01-07 16:07:08 +0000

    Remove InternalTx channel from Proxy

--- src/proxy/proxy.go
+++ src/proxy/proxy.go
@@ -6,7 +6,6 @@ import (
 
 type AppProxy interface {
 	SubmitCh() chan []byte
-	SubmitInternalCh() chan hashgraph.InternalTransaction
 	CommitBlock(block hashgraph.Block) (CommitResponse, error)
 	GetSnapshot(blockIndex int) ([]byte, error)
 	Restore(snapshot []byte) error
--- src/proxy/socket/app/socket_app_proxy_server.go
+++ src/proxy/socket/app/socket_app_proxy_server.go
@@ -5,23 +5,20 @@ import (
 	"net/rpc"
 	"net/rpc/jsonrpc"
 
-	"github.com/mosaicnetworks/babble/src/hashgraph"
 	"github.com/sirupsen/logrus"
 )
 
 type SocketAppProxyServer struct {
-	netListener      *net.Listener
-	rpcServer        *rpc.Server
-	submitCh         chan []byte
-	submitInternalCh chan hashgraph.InternalTransaction
-	logger           *logrus.Logger
+	netListener *net.Listener
+	rpcServer   *rpc.Server
+	submitCh    chan []byte
+	logger      *logrus.Logger
 }
 
 func NewSocketAppProxyServer(bindAddress string, logger *logrus.Logger) (*SocketAppProxyServer, error) {
 	server := &SocketAppProxyServer{
-		submitCh:         make(chan []byte),
-		submitInternalCh: make(chan hashgraph.InternalTransaction),
-		logger:           logger,
+		submitCh: make(chan []byte),
+		logger:   logger,
 	}
 
 	if err := server.register(bindAddress); err != nil {
@@ -67,16 +64,6 @@ func (p *SocketAppProxyServer) SubmitTx(tx []byte, ack *bool) error {
 	p.logger.Debug("SubmitTx")
 
 	p.submitCh <- tx
-
-	*ack = true
-
-	return nil
-}
-
-func (p *SocketAppProxyServer) SubmitInternalTx(tx hashgraph.InternalTransaction, ack *bool) error {
-	p.logger.Debug("SubmitTx")
-
-	p.submitInternalCh <- tx
 
 	*ack = true
 
