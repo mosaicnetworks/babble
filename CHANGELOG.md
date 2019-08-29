05fad5e22185549c327cd016d05ba5d20c9d3f04
arrivets <arrivets@gmail.com>
2019-08-29 14:20:19 +0100

    Bump version 0.5.2 -> 0.5.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Change Log
 
-## Unreleased
+## v0.5.3 (August 29, 2019)
 
 FEATURES:
 
--- src/version/version.go
+++ src/version/version.go
@@ -3,11 +3,11 @@ package version
 // Flag contains extra info about the version. It is helpul for tracking
 // versions while developing. It should always by empty on the master branch.
 // This will be inforced in a continuous integration test.
-const Flag = "develop"
+const Flag = ""
 
 var (
 	// Version is The full version string
-	Version = "0.5.2"
+	Version = "0.5.3"
 
 	// GitCommit is set with --ldflags "-X main.gitCommit=$(git rev-parse HEAD)"
 	GitCommit string
