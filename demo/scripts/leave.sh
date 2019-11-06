a4522dfa344366a53548c5c614b1ad34e27103a2
arrivets <arrivets@gmail.com>
2019-11-06 12:36:12 +0000

    Fix the demo leave script

--- demo/scripts/leave.sh
+++ demo/scripts/leave.sh
@@ -2,4 +2,11 @@
 
 NODE=${1:-1}
 
-docker kill --signal=SIGINT node$NODE client$NODE
+# If we kill the client before the node is properly shutdown, it will get 
+# invalid block signatures as it tries to commit blocks with empty state-hashes.
+# So we send the SIGINT signal the node, give it 5 seconds to politely leave,
+# and kill the client.
+
+docker kill --signal=SIGINT node$NODE && \
+sleep 5 && \
+docker kill client$NODE 
