a2a04eda77b234b3f341d98a95572ef3a05e6dd8
arrivets <arrivets@gmail.com>
2020-04-13 10:20:30 +0100

    Document Graph

--- src/node/graph.go
+++ src/node/graph.go
@@ -4,19 +4,29 @@ import (
 	hg "github.com/mosaicnetworks/babble/src/hashgraph"
 )
 
-//Infos is a struct providing Hashgraph information
+// Infos is the object used by Graph to collect information about a Hashgraph.
 type Infos struct {
 	ParticipantEvents map[string]map[string]*hg.Event
 	Rounds            []*hg.RoundInfo
 	Blocks            []*hg.Block
 }
 
-//Graph is a struct containing a node
+// Graph is a struct containing a node which is is used to collect information
+// about the underlying Hashgraph in view of producing a visual representation
+// of the Hashgraph.
 type Graph struct {
 	*Node
 }
 
-//GetParticipantEvents returns Participant Events
+// NewGraph instantiates a Graph from a Node.
+func NewGraph(n *Node) *Graph {
+	return &Graph{
+		Node: n,
+	}
+}
+
+// GetParticipantEvents returns all the Events for all the participants that
+// have been members of the group.
 func (g *Graph) GetParticipantEvents() (map[string]map[string]*hg.Event, error) {
 	res := make(map[string]map[string]*hg.Event)
 
@@ -56,7 +66,7 @@ func (g *Graph) GetParticipantEvents() (map[string]map[string]*hg.Event, error) 
 	return res, nil
 }
 
-//GetRounds returns an array of RoundInfo
+// GetRounds returns all the recorded Hashgraph rounds.
 func (g *Graph) GetRounds() []*hg.RoundInfo {
 	res := []*hg.RoundInfo{}
 
@@ -79,7 +89,7 @@ func (g *Graph) GetRounds() []*hg.RoundInfo {
 	return res
 }
 
-//GetBlocks returns an array of Blocks
+// GetBlocks returns all the recorded blocks.
 func (g *Graph) GetBlocks() []*hg.Block {
 	res := []*hg.Block{}
 
@@ -102,7 +112,7 @@ func (g *Graph) GetBlocks() []*hg.Block {
 	return res
 }
 
-//GetInfos returns an Infos struct
+// GetInfos returns an Infos struct representing the entire Hashgraph.
 func (g *Graph) GetInfos() (Infos, error) {
 	participantEvents, err := g.GetParticipantEvents()
 	if err != nil {
@@ -114,11 +124,4 @@ func (g *Graph) GetInfos() (Infos, error) {
 		Rounds:            g.GetRounds(),
 		Blocks:            g.GetBlocks(),
 	}, nil
-}
-
-//NewGraph is a factory method returning a Graph
-func NewGraph(n *Node) *Graph {
-	return &Graph{
-		Node: n,
-	}
 }
